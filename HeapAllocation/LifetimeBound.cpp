c57dba53d9e4abcd7e45f93bad60f2678dde1542
benedekaibas <kaibas01@allegheny.edu>
2026-07-20 14:43:36 +0200

    [test] Rename variable.

--- HeapAllocation/LifetimeBound.cpp
+++ HeapAllocation/LifetimeBound.cpp
@@ -1,10 +1,10 @@
 int *test_func(int *p [[clang::lifetimebound]]);
 
-void heap_dangling_source_lifetimebound() {
-  int *heap_i = new int(5);
-  int *p = test_func(heap_i);
-  delete heap_i;
-  (void)p;
+int *heap_dangling_source_lifetimebound() {
+  int *i = new int(5);
+  int *p = test_func(i);
+  delete i;
+  return p;
 }
 
 /*
